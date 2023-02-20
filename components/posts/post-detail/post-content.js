be9e871c5d961f38e4cf2f2cab0a5b56a12b9638
Alan Grądecki <alangradecki@wp.pl>
2023-02-20 21:37:41 +0100

    rendering dynamics post pages and paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,14 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting started with NextJS!",
-  image: "getting-started-nextjs.png",
-  date: "2023-02-20",
-  content: "# This is a first post",
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
