8ae3675a708263c2549a8759cb2f927ec8f3d4cb
Alan Grądecki <alangradecki@wp.pl>
2023-02-20 11:14:51 +0100

    added css styles

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,9 +1,11 @@
 import Logo from "./logo";
 import Link from "next/link";
 
+import classes from "./main-navigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href="/">
         <a>
           <Logo />
