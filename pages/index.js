2d60a9bf7c1453c3d01de885740cf77e5dfd333c
Alan Grądecki <alangradecki@wp.pl>
2023-02-20 20:36:42 +0100

    implemented rendering posts using markdown data

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,25 @@
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
-const DUMMY_POSTS = [
-  {
-    title: "Getting started with NextJS!",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS ia the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    slug: "getting-started-with-nextjs",
-    date: "2023-02-20",
-  },
-  {
-    title: "Getting started with NextJS!",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS ia the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    slug: "getting-started-with-nextjs2",
-    date: "2023-02-20",
-  },
-  {
-    title: "Getting started with NextJS!",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS ia the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    slug: "getting-started-with-nextjs3",
-    date: "2023-02-20",
-  },
-  {
-    title: "Getting started with NextJS!",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS ia the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    slug: "getting-started-with-nextjs4",
-    date: "2023-02-20",
-  },
-];
+import { getFeaturedPosts } from "../lib/posts-util";
 
-function HomePage() {
+function HomePage(props) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
